be90d7e71e76b045f47a3537d65d7b27d339c3ad
Diego Auyón <diegoeauyonh@gmail.com>
2022-05-31 20:03:19 -0600

    feat(deploy), add deployer

---
+++ deploy.php
@@ -0,0 +1,58 @@
+<?php
+namespace Deployer;
+
+require 'recipe/symfony4.php';
+
+// User chmod whie we learn about acl.
+set('writable_mode', 'acl'); // chmod, chown, chgrp or acl.
+
+// Project name
+set('application', 'servicios.publicos');
+
+// Project repository
+set('repository', 'git@github.com:RedCiudadana/ServiciosPublicosAdmin.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true); 
+
+// Shared files/dirs between deploys 
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server 
+add('writable_dirs', []);
+
+
+// Hosts
+
+host('servicios-ocean')
+    ->hostname('142.93.77.143')
+    ->user('redciudadana')
+    ->set('remote_user', 'redciudadana')
+    ->set('deploy_path', '/srv/web-apps/admin.tramites.redciudadana.org');
+    
+// Tasks
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+after('deploy:vendors', 'deploy:vendors_js');
+
+task('deploy:vendors_js', function () {
+    run('cd {{release_path}} && npm install');
+});
+
+after('deploy:vendors_js', 'deploy:node');
+
+task('deploy:node', function () {
+    run('cd {{release_path}} && npm run build');
+});
+
+// Migrate database before symlink new release.
+
+before('deploy:symlink', 'database:migrate');
+
